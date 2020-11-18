2c7ecddbb6f573e54ef8ab4385e53a4344d96eda
Yunso-yeon <yunsy7854@gmail.com>
2020-11-19 08:24:44 +0900

    Updates

---
+++ precache-manifest.f82fda5b9daaa8bdfda5563320146c47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3c47c5c9b03c4a5b2d47619c89ee0a0",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "97d9a12814351c4108c9",
+    "url": "/movie_app_2020/static/css/main.fb0faf07.chunk.css"
+  },
+  {
+    "revision": "3dc07eb8fe714f672825",
+    "url": "/movie_app_2020/static/js/2.7ab01957.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/movie_app_2020/static/js/2.7ab01957.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97d9a12814351c4108c9",
+    "url": "/movie_app_2020/static/js/main.00ecc7ca.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
